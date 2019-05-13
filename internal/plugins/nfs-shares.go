0095df5779886d1bb70adde1bd2ae347083dc546
Stefan Majewsky <stefan.majewsky@sap.com>
2019-05-13 15:14:39 +0200

    start implementing the nfs-shares asset manager

---
+++ internal/plugins/nfs-shares.go
@@ -0,0 +1,101 @@
+/******************************************************************************
+*
+*  Copyright 2019 SAP SE
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+*
+******************************************************************************/
+
+package plugins
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/gophercloud/gophercloud"
+	"github.com/gophercloud/gophercloud/openstack"
+	"github.com/gophercloud/gophercloud/openstack/sharedfilesystems/v2/shares"
+	"github.com/sapcc/castellum/internal/core"
+	"github.com/sapcc/castellum/internal/db"
+)
+
+type assetManagerNFS struct {
+	Manila *gophercloud.ServiceClient
+}
+
+func init() {
+	core.RegisterAssetManagerFactory("nfs-shares", func(provider *gophercloud.ProviderClient, eo gophercloud.EndpointOpts) (core.AssetManager, error) {
+		manila, err := openstack.NewSharedFileSystemV2(provider, eo)
+		if err != nil {
+			return nil, err
+		}
+		manila.Microversion = "2.23"
+		return &assetManagerNFS{manila}, nil
+	})
+}
+
+//AssetTypes implements the core.AssetManager interface.
+func (m *assetManagerNFS) AssetTypes() []db.AssetType {
+	return []db.AssetType{"nfs-shares"}
+}
+
+//ListAssets implements the core.AssetManager interface.
+func (m *assetManagerNFS) ListAssets(res db.Resource) ([]string, error) {
+	page := 0
+	pageSize := 250
+	var shareIDs []string
+
+	//TODO: simplify this by adding a shares.List() function to
+	//package github.com/gophercloud/gophercloud/openstack/sharedfilesystems/v2/shares
+	for {
+		url := m.Manila.ServiceURL("shares") + fmt.Sprintf("?project_id=%s&all_tenants=1&limit=%d&offset=%d", res.ScopeUUID, pageSize, page*pageSize)
+		var r gophercloud.Result
+		_, err := m.Manila.Get(url, &r.Body, nil)
+		if err != nil {
+			return nil, err
+		}
+
+		var data struct {
+			Shares []struct {
+				ID string `json:"id"`
+			} `json:"shares"`
+		}
+		err = r.ExtractInto(&data)
+		if err != nil {
+			return nil, err
+		}
+
+		if len(data.Shares) > 0 {
+			for _, share := range data.Shares {
+				shareIDs = append(shareIDs, share.ID)
+			}
+			page++
+		} else {
+			//last page reached
+			return shareIDs, nil
+		}
+	}
+}
+
+//SetAssetSize implements the core.AssetManager interface.
+func (m *assetManagerNFS) SetAssetSize(res db.Resource, assetUUID string, oldSize, newSize uint64) error {
+	if oldSize < newSize {
+		return shares.Extend(m.Manila, assetUUID, shares.ExtendOpts{NewSize: int(newSize)}).ExtractErr()
+	}
+	return shares.Shrink(m.Manila, assetUUID, shares.ShrinkOpts{NewSize: int(newSize)}).ExtractErr()
+}
+
+//GetAssetStatus implements the core.AssetManager interface.
+func (m *assetManagerNFS) GetAssetStatus(res db.Resource, assetUUID string, previousStatus *core.AssetStatus) (core.AssetStatus, error) {
+	return core.AssetStatus{}, errors.New("TODO implement")
+}
---
+++ vendor/github.com/sapcc/go-bits/assert/http.go
@@ -0,0 +1,95 @@
+/*******************************************************************************
+*
+* Copyright 2017-2018 SAP SE
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You should have received a copy of the License along with this
+* program. If not, you may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*******************************************************************************/
+
+package assert
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+//HTTPRequestBody is the type of field HTTPRequest.RequestBody.
+//It is implemented by StringData and JSONObject.
+type HTTPRequestBody interface {
+	GetRequestBody() (io.Reader, error)
+}
+
+//HTTPResponseBody is the type of field HTTPRequest.ExpectBody.
+//It is implemented by StringData and JSONObject.
+type HTTPResponseBody interface {
+	//Checks that the given actual response body is equal to this expected value.
+	//`request` contains a user-readable representation of the original request,
+	//for use in error messages.
+	AssertResponseBody(t *testing.T, requestInfo string, responseBody []byte)
+}
+
+//HTTPRequest is a HTTP request that gets executed by a unit test.
+type HTTPRequest struct {
+	//request properties
+	Method string
+	Path   string
+	Header map[string]string
+	Body   HTTPRequestBody
+	//response properties
+	ExpectStatus int
+	ExpectBody   HTTPResponseBody
+}
+
+//Check performs the HTTP request described by this HTTPRequest against the
+//given http.Handler and compares the response with the expectations in the
+//HTTPRequest.
+func (r HTTPRequest) Check(t *testing.T, handler http.Handler) {
+	t.Helper()
+
+	var requestBody io.Reader
+	if r.Body != nil {
+		var err error
+		requestBody, err = r.Body.GetRequestBody()
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+	request := httptest.NewRequest(r.Method, r.Path, requestBody)
+	if r.Header != nil {
+		for key, value := range r.Header {
+			request.Header.Set(key, value)
+		}
+	}
+
+	recorder := httptest.NewRecorder()
+	handler.ServeHTTP(recorder, request)
+
+	response := recorder.Result()
+	responseBytes, _ := ioutil.ReadAll(response.Body)
+
+	if response.StatusCode != r.ExpectStatus {
+		t.Errorf("%s %s: expected status code %d, got %d",
+			r.Method, r.Path, r.ExpectStatus, response.StatusCode,
+		)
+	}
+
+	if r.ExpectBody != nil {
+		requestInfo := fmt.Sprintf("%s %s", r.Method, r.Path)
+		r.ExpectBody.AssertResponseBody(t, requestInfo, responseBytes)
+	}
+}
